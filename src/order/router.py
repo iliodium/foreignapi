67178fe09ed7f149acaa3f3698f9f7636cb8036e
iliodium <95029175+iliodium@users.noreply.github.com>
2024-02-27 10:09:31 +0300

    add rabbitmq

--- src/order/router.py
+++ src/order/router.py
@@ -1,16 +1,17 @@
-import pickle
 import json
+import pickle
+import random
 
 import aiohttp
-from sqlalchemy import select
+import pika
 from fastapi import APIRouter, Depends
 from google.protobuf.json_format import MessageToJson
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
 
 from src.config import BOOKKEEPING_REST_NAME, BOOKKEEPING_REST_PORT
 from src.database import get_async_session
 from src.models import CustomerOrm
-from sqlalchemy.ext.asyncio import AsyncSession
-
 from src.services.bookkeeping_service_grpc.client import BookkeepingServiceClient as BookkeepingGRPC
 from src.utils import check_exist_customer
 
@@ -20,6 +21,13 @@ router = APIRouter(
 )
 
 aiohttp_session = aiohttp.ClientSession()
+bookkeeping = BookkeepingGRPC()
+
+credentials = pika.PlainCredentials('user', 'user')
+parameters = pika.ConnectionParameters('rabbitmq',
+                                       5672,
+                                       '/',
+                                       credentials)
 
 
 @router.get("/v1")
@@ -43,7 +51,6 @@ async def get_order(customer_name: str = "Заведение3",
                                    data=pickle.dumps(check)) as resp:
         result_rest: str = await resp.text()
 
-    bookkeeping = BookkeepingGRPC()
     response_grpc = bookkeeping.send_check(check)
     result_grpc = json.loads(MessageToJson(response_grpc))
 
@@ -55,5 +62,19 @@ async def get_order(customer_name: str = "Заведение3",
     check["summa"] = summa
     check["summa_after_tax_rest"] = int(result_rest) / 100
     check["summa_after_tax_grpc"] = result_grpc['tax'] / 100
+
+    connection_rabbitmq = pika.BlockingConnection(parameters)
+    channel_rabbitmq = connection_rabbitmq.channel()
+
+    rb_keys = ['expensive', 'cheap']
+    channel_rabbitmq.basic_publish(exchange='my_exchange',
+                                   routing_key=f"{random.choice(rb_keys)}.some_key",
+                                   body=str(check["summa"]).encode('utf-8'),
+                                   properties=pika.BasicProperties(
+                                       delivery_mode=pika.DeliveryMode.Persistent)
+                                   )
+
+    channel_rabbitmq.close()
+    connection_rabbitmq.close()
 
     return check
